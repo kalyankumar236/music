a2ce3d0ec0e9b4f95e9114b475fd6b663eaf853c
kalyankumar236 <49588052+kalyankumar236@users.noreply.github.com>
2021-02-16 13:10:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+var n = document.querySelectorAll(".drum").length;
+for (var i = 0; i < n; i++) {
+  document.querySelectorAll('.drum')[i].addEventListener("click", function() {
+    var button = this.innerHTML;
+    makeSound(button);
+    addAnimation(button);
+  }
+);
+}
+    document.addEventListener("keypress",function(event){
+    makeSound(event.key);
+    addAnimation(event.key);
+  });
+  function makeSound(key){
+    switch (key) {
+      case "w":
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+        break;
+      case "a":
+        var tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+        break;
+      case "s":
+        var tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+        break;
+      case "d":
+        var tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+        break;
+      case "k":
+        var crash = new Audio("sounds/crash.mp3");
+        crash.play();
+        break;
+      case "l":
+        var kickbass = new Audio("sounds/kick-bass.mp3");
+        kickbass.play();
+        break;
+      case "j":
+      var snare=new Audio("sounds/snare.mp3");
+      snare.play();
+        break;
+      default:
+    }
+}
+function addAnimation(currentkey)
+{
+var activekey=  document.querySelector("."+ currentkey);
+activekey.classList.add("pressed");
+setTimeout(function() {
+
+activekey.classList.remove("pressed");},100);
+}
